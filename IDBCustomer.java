d3cf3cb06e00ccb93f411512572056ba7ad6e443
MartinHvizdak <71123628+MartinHvizdak@users.noreply.github.com>
2021-05-15 20:47:48 +0200

    deleting files outside of folders

--- IDBCustomer.java
+++
@@ -1,8 +0,0 @@
-package db;
-
-import model.Customer;
-
-public interface IDBCustomer {
-		 	public Customer retrieveCustomerByEmail(String customerEmail) throws DBException;
-
-}
--- IDBService.java
+++
@@ -1,9 +0,0 @@
-package db;
-
-import model.Service;
-
-import java.util.ArrayList;
-
-public interface IDBService {
-    public ArrayList<Service> retrieveAllServices() throws DBException;
-}
