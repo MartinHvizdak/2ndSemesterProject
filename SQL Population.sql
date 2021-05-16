c667e370fea658f9f20ae209124e1bbe6f8b7563
MartinHvizdak <71123628+MartinHvizdak@users.noreply.github.com>
2021-05-16 11:17:28 +0200

    sql creation and population

---
+++ SQL Population.sql
@@ -0,0 +1,56 @@
+insert into Cities
+values
+('1000', 'Copenhagen'),
+('2200', 'Aarhus'),
+('9000', 'Aalborg')
+
+insert into Customers
+values
+('customer1@gmail.com', 'Hobrovej', '11', 'FisherMaster', '23452345', '1000', 'Private_individual'),
+('customer2@gmail.com', 'Hobrovej', '3', 'FisherKokot', '87654321', '2200', 'Self_employeed'),
+('customer3@gmail.com', 'Hobrovej', '2', 'FisherNoob', '33445566', '9000', 'LTD')
+
+insert into Orders
+values
+('111', 'customer1@gmail.com', '1-1-2001'),
+('222', 'customer2@gmail.com', '2-2-2002'),
+('333', 'customer3@gmail.com', '3-3-2003')
+
+insert into Services
+values
+('a', 'service a', '101'),
+('b', 'service b', '202'),
+('c', 'service c', '303')
+
+insert into Order_lines
+values
+('111', 'a', '1'),
+('222', 'a', '1'),
+('222', 'b', '2'),
+('333', 'a', '1'),
+('333', 'b', '2'),
+('333', 'c', '3')
+
+insert into Private_individuals
+values
+('customer1@gmail.com', '1234', '23DG')
+
+insert into Self_employeed
+values
+('customer2@gmail.com', '5678', '67JR')
+
+insert into LTDs
+values
+('customer3@gmail.com', '9087', '5275', 'True')
+
+insert into LTD_employees
+values
+('1', 'Lorence', 'Kokot', '2143', '133700', 'customer3@gmail.com')
+
+insert into LTD_owners
+values
+('123', 'Jordan', 'Jordon', 'Unhappy marriage')
+
+insert into LTD_ownerships
+values
+('customer3@gmail.com', '123')
--- SQL creation.sql
+++ SQL creation.sql
@@ -1,3 +1,6 @@
+use SecondSemesterProject;
+
+
 create table Services(
 name varchar(50) NOT NULL,
 description varchar(250) NOT NULL,
@@ -7,27 +10,33 @@ primary key(name)
 
 create table Order_lines(
 order_id int NOT NULL,
-service_name unique varchar(50) NOT NULL,
+service_name varchar(50) NOT NULL,
 quantity int NOT NULL,
 primary key(order_id, service_name)
 );
 
 create table Orders(
 id int NOT NULL,
-customer_email varchar(100) NOT NULL,
+customer_email varchar(100) NOT NULL default('Customer does not exist'),
 payday date NOT NULL,
 primary key(id)
 );
 
 create table Customers(
 email varchar(100) NOT NULL,
-city varchar(30) NOT NULL,
 street varchar(50) NOT NULL,
 street_number varchar(10) NOT NULL,
-zip_code varchar(10) NOT NULL,
 name varchar(50) NOT NULL,
 phone_number int NOT NULL,
+zip_code varchar(10) NOT NULL,
+customer_type varchar(100) NOT NULL,
 primary key(email)
+);
+
+create table Cities(
+zip_code varchar(10) NOT NULL,
+city varchar(100) NOT NULL,
+primary key(zip_code)
 );
 
 create table Private_individuals(
@@ -46,7 +55,7 @@ primary key(customer_email)
 
 create table LTDs(
 customer_email varchar(100) NOT NULL,
-market_register_number varchar(15) NOT NULL,
+market_registration_number varchar(15) NOT NULL,
 market_number varchar(15) NOT NULL,
 are_payers bit NOT NULL,
 primary key(customer_email)
@@ -62,7 +71,7 @@ LTD_email varchar(100) NOT NULL,
 primary key(id)
 );
 
-create table Owned_LTDs(
+create table LTD_ownerships(
 LTD_email varchar(100) NOT NULL,
 owner_id varchar(10) NOT NULL,
 primary key(LTD_email, owner_id)
@@ -76,12 +85,13 @@ relation varchar(50) NOT NULL,
 primary key(id)
 );
 
+alter table Customers add foreign key(zip_code) references Cities(zip_code);
+alter table order_lines add foreign key(order_id) references Orders(id) on delete cascade;
 alter table order_lines add foreign key(service_name) references Services(name);
-alter table order_lines add foreign key(order_id) references Orders(id);
 alter table Orders add foreign key(customer_email) references Customers(email);
 alter table private_individuals add foreign key(customer_email) references Customers(email);
 alter table self_employeed add foreign key(customer_email) references Customers(email);
 alter table LTDs add foreign key(customer_email) references Customers(email);
 alter table LTD_employees add foreign key(LTD_email) references LTDs(customer_email);
-alter table Owned_LTDs add foreign key(LTD_email) references LTDs(customer_email);
-alter table Owned_LTDs add foreign key(owner_id) references LTD_owners(id);
+alter table LTD_ownerships add foreign key(LTD_email) references LTDs(customer_email);
+alter table LTD_ownerships add foreign key(owner_id) references LTD_owners(id);
