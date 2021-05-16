cb6d270a12efc4d63f91008dfef00978ae13171d
Alis-33 <73468599+Alis-33@users.noreply.github.com>
2021-05-16 04:25:05 +0200

    Update CustomerController.java

--- controller/CustomerController.java
+++ controller/CustomerController.java
@@ -2,6 +2,14 @@ package controller;
 
 import db.DBException;
 import model.Customer;
+import model.CustomerEmployee;
+import model.LTD;
+import model.Owner;
+import model.PrivateIndividual;
+import model.SelfEmployeed;
+
+import java.util.ArrayList;
+
 import db.DBCustomer;
 
 public class CustomerController {
@@ -22,4 +30,25 @@ public class CustomerController {
     public Customer createCustomer(){
         return new Customer();
     }
+    
+    
+    public boolean savePrivateIndividualWithUserInputInDB(String name, String city, String street, String streetNumber, String zipCode, String email, String phoneNumber, String ID, String VAT) throws DBException {
+    	PrivateIndividual privateIndividual = new PrivateIndividual(email, name, phoneNumber, city, zipCode, street, streetNumber, ID, VAT);
+    	
+    	return dbCustomer.savePrivateIndividualWithUserInputInDB(privateIndividual);
+     }
+    
+	 public boolean saveSelfEmployedWithUserInputInDB(String name, String city, String street, String streetNumber, String zipCode, String email, String phoneNumber, String VAT, String marketNumber) throws DBException{
+	    SelfEmployeed selfEmployed = new SelfEmployeed(email, name, phoneNumber, city, zipCode, street, streetNumber, VAT, marketNumber);
+	    
+
+    	return dbCustomer.saveSelfEmployedWithUserInputInDB(selfEmployed);
+	 }
+	 
+	 public boolean saveLTDUserInputInDB(String name, String city, String street, String streetNumber, String zipCode, String email, String phoneNumber, ArrayList<CustomerEmployee> employees, ArrayList<Owner> owners, String marketRegisteredNumber, String marketNumber, boolean arePayers) throws DBException{
+	 	LTD ltd = new LTD(email, name, phoneNumber, city, zipCode, street, streetNumber, employees, owners, marketRegisteredNumber, marketNumber, arePayers);
+	 	
+
+    	return dbCustomer.saveLTDUserInputInDB(ltd);
+	 }
 }
